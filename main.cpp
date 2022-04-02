ae5a1922896d65faa1334ac52ac4d097f9dba63f
AlbertoCarena <100611068+AlbertoCarena@users.noreply.github.com>
2022-04-02 18:56:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main() {
+   float a,b;
+   cin>> a >>b;
+   int opzione;
+   cin>> opzione;
+   float Atriangolo=0, Aquadrato=0, Arettangolo=0;
+   switch( opzione ) {
+      case 0:
+         Atriangolo= ((a*b)/2);
+         cout<<Atriangolo<<endl;
+         break;
+      case 1:
+         Aquadrato= (a*a);
+         cout<<Aquadrato<<endl;
+         break;
+      case 2:
+         Arettangolo= (a*b);
+         cout<<Arettangolo<<endl;
+      default:
+         cout<< "opzione non valida" <<endl;
+   }
    return 0;
 }
